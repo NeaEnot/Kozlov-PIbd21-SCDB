4639a7b1d8e63af1864bc2c6501a1e2cd9e176bc
NeaEnot <oleg.kozloss@yandex.ru>
2020-03-30 22:04:33 +0400

    Read (CRUD)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using System;
 using System.Linq;
@@ -21,6 +22,7 @@ namespace Lab5
                         Create();
                         break;
                     case "read":
+                        Read();
                         break;
                     case "update":
                         break;
@@ -251,6 +253,230 @@ namespace Lab5
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Read()
+        {
+            Console.Write("Enter entity: ");
+            string entity = Console.ReadLine();
+
+            switch(entity.ToLower())
+            {
+                case "brigade":
+                    {
+                        Console.Write("Enter order id: ");
+                        int orderId = Int32.Parse(Console.ReadLine());
+
+                        var brigades =
+                            db.Brigades
+                            .Where(rec => rec.OrderId == orderId)
+                            .Select(rec => new Brigade
+                            {
+                                Id = rec.Id,
+                                WorkTypeId = rec.WorkTypeId,
+                                OrderId = rec.OrderId == null ? null : rec.OrderId,
+                                WorkType = db.WorkTypes.Include(recWT => recWT.Id == rec.WorkTypeId).FirstOrDefault()
+                            });
+
+                        Console.WriteLine("Brigade id\tWork type");
+                        Console.WriteLine();
+                        foreach (var brigade in brigades)
+                        {
+                            Console.WriteLine("{0}\t{1}", brigade.Id, brigade.WorkType.Name);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "materialsset":
+                    {
+                        Console.Write("Enter order id: ");
+                        int orderId = Int32.Parse(Console.ReadLine());
+
+                        var materialsSets =
+                            db.MaterialsSets
+                            .Where(rec => rec.OrderId == orderId)
+                            .Select(rec => new MaterialsSet
+                            {
+                                Id = rec.Id,
+                                Count = rec.Count,
+                                MaterialsTypeId = rec.MaterialsTypeId,
+                                OrderId = rec.OrderId,
+                                MaterialsType = db.MaterialsTypes.Include(recMT => recMT.Id == rec.MaterialsTypeId).FirstOrDefault()
+                            });
+
+                        Console.WriteLine("Materials type\tCount");
+                        Console.WriteLine();
+                        foreach (var set in materialsSets)
+                        {
+                            Console.WriteLine("{0}\t{1}", set.MaterialsType.Name, set.Count);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "materialstype":
+                    {
+                        var materialsTypes =
+                            db.MaterialsTypes
+                            .Select(rec => new MaterialsType
+                            {
+                                Id = rec.Id,
+                                Name = rec.Name,
+                                PricePerUnit = rec.PricePerUnit
+                            });
+
+                        Console.WriteLine("Materials type\tPrice per unit");
+                        Console.WriteLine();
+                        foreach (var type in materialsTypes)
+                        {
+                            Console.WriteLine("{0}\t{1}", type.Name, type.PricePerUnit);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "order":
+                    {
+                        var orders =
+                            db.Orders
+                            .Select(rec => new Order
+                            {
+                                Id = rec.Id,
+                                RegistrationDate = rec.RegistrationDate,
+                                DeliveryDate = rec.DeliveryDate
+                            });
+
+                        Console.WriteLine("Order id\tRegistration date\tDelivery date");
+                        Console.WriteLine();
+                        foreach (var order in orders)
+                        {
+                            Console.WriteLine("{0}\t{1}\t{2}", order.Id, order.RegistrationDate.ToString("dd:MM:yyyy"), order.DeliveryDate.ToString("dd:MM:yyyy"));
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "position":
+                    {
+                        var positions =
+                            db.Positions
+                            .Select(rec => new Position
+                            {
+                                Id = rec.Id,
+                                Name = rec.Name,
+                                Salary = rec.Salary
+                            });
+
+                        Console.WriteLine("Position\tSalary");
+                        Console.WriteLine();
+                        foreach (var position in positions)
+                        {
+                            Console.WriteLine("{0}\t{1}", position.Name, position.Salary);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "work":
+                    {
+                        Console.Write("Enter order id: ");
+                        int orderId = Int32.Parse(Console.ReadLine());
+
+                        var works =
+                            db.Works
+                            .Where(rec => rec.OrderId == orderId)
+                            .Select(rec => new Work
+                            {
+                                Id = rec.Id,
+                                WorkTypeId = rec.WorkTypeId,
+                                OrderId = rec.OrderId,
+                                WorkType = db.WorkTypes.Include(recWT => recWT.Id == rec.WorkTypeId).FirstOrDefault()
+                            });
+
+                        Console.WriteLine("Work");
+                        Console.WriteLine();
+                        foreach (var work in works)
+                        {
+                            Console.WriteLine(work.WorkType.Name);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "worker":
+                    {
+                        Console.Write("Enter brigade id: ");
+                        int brigadeId = Int32.Parse(Console.ReadLine());
+
+                        var workers =
+                            db.Workers
+                            .Where(rec => rec.BrigadeId == brigadeId)
+                            .Select(rec => new Worker
+                            {
+                                Id = rec.Id,
+                                FirstName = rec.FirstName,
+                                SecondName = rec.SecondName,
+                                LastName = rec.LastName,
+                                BirthDate = rec.BirthDate,
+                                AdmissionDate = rec.AdmissionDate,
+                                PositionId = rec.PositionId,
+                                BrigadeId = rec.BrigadeId,
+                                Position = db.Positions.Include(recP => recP.Id == rec.PositionId).FirstOrDefault()
+                            });
+
+                        Console.WriteLine("Worker\tPosition\tAge\tAdmission date");
+                        Console.WriteLine();
+                        foreach (var worker in workers)
+                        {
+                            Console.WriteLine("{0} {1}.{2}.\t{3}\t{4}\t{5}", worker.SecondName, worker.FirstName[0], worker.LastName[0], DateTime.Now.Year - worker.BirthDate.Year, worker.AdmissionDate.ToString("dd:MM:yyyy"));
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                case "worktype":
+                    {
+                        var workTypes =
+                            db.WorkTypes
+                            .Select(rec => new WorkType
+                            {
+                                Id = rec.Id,
+                                Name = rec.Name
+                            });
+
+                        Console.WriteLine("Work type");
+                        Console.WriteLine();
+                        foreach (var type in workTypes)
+                        {
+                            Console.WriteLine(type.Name);
+                        }
+
+                        Console.WriteLine();
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Unknown entity: " + entity);
+                        break;
+                    }
             }
         }
     }
